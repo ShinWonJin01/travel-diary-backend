1949f785aa91d9e8e67e1c3979cf8e52feddb6c6
Wonjin Shin <dnjswls151@naver.com>
2026-07-31 08:57:00 +0900

    fix: 모바일 접속을 위한 CORS 설정 수정

--- src/main/java/com/shinwonjin/traveldiary/config/CorsConfig.java
+++ src/main/java/com/shinwonjin/traveldiary/config/CorsConfig.java
@@ -10,7 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOriginPatterns(
+                        "http://localhost:5173",
+                        "http://*:5173"
+                )
                 .allowedMethods(
                         "GET",
                         "POST",
